8fa7bc5b9a352f50cf574299ba2b5d1ea61e7609
jojo989 <yolo.33366@gmail.com>
2024-03-06 18:47:53 -0300

    update xd

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <Geode/Geode.hpp>
 #include <Geode/modify/CCLabelBMFont.hpp>
 #include <cctype>
+#include <regex>
 
 using namespace geode::prelude;
 
@@ -8,16 +9,31 @@ class $modify(CCLabelBMFont) {
     static CCLabelBMFont* create(char const* str, char const* fntFile) {
         std::string owoStr(str);
 
-        // idk if this is the best way to do this 
-        for (char& c : owoStr) {
-            if (std::tolower(c) == 'r' || std::tolower(c) == 'l') {
-                c = 'w';
-            }
+        //stolen from uwufier, idk if this is the best way to do this >~<
+        std::vector<std::pair<std::regex, std::string>> uwuMap = {
+            {std::regex("(?:R|L)"), "W"},
+            {std::regex("(?:r|l)"), "w"},
+            {std::regex("n([aeiou])"), "ny$1"},
+            {std::regex("N([aeiou])"), "Ny$1"},
+            {std::regex("N([AEIOU])"), "Ny$1"},
+            {std::regex("ove"), "uv"},
+            {std::regex("Tiny"), "Tynie :>"},
+            {std::regex("Showt"), "Smol :3"},
+            {std::regex("XW"), "Too big >_<"},
+            {std::regex("Wong"), "Looong :O"} //lmao
+            
+        };
+
+
+
+        for (auto& rule : uwuMap) {
+            owoStr = std::regex_replace(owoStr, rule.first, rule.second);
         }
+
+
         bool owoConfig = Mod::get()->getSettingValue<bool>("owoo");
         if (owoConfig)  return CCLabelBMFont::create((owoStr + " uwu").c_str(), fntFile);
 
         return CCLabelBMFont::create(owoStr.c_str(), fntFile);
-        
     }
 };
